87f2c10895cbee128c0f12ef3ab17a3cc2aed1d6
Om-Sureja <omsureja@gmail.com>
2024-03-21 21:40:49 +0530

    path code

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const userRouter = require("./routes/user");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const database = require("./config/database");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +15,12 @@ dotenv.config();
 database.connect();
 
 app.use("/user", userRouter);
+
+app.use(express.static("public"));
+app.get("/*", (req, res) => {
+  const filePath = path.join(__dirname, "public", "index.html");
+  res.sendFile(filePath);
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
